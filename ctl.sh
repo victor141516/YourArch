2176c7f4cc0437116570ceed82ae1ee937cba2db
Victor <yomellamovictor@gmail.com>
2022-01-07 02:54:57 +0100

    --wip-- [skip ci]

--- ctl.sh
+++ ctl.sh
@@ -4,15 +4,17 @@ env=$1
 cmd="docker-compose -f docker-compose.base.yml -f docker-compose.$env.yml"
 
 if [ "$2" = "logs" ]; then
-  $cmd logs -f yourarch
+  run="$cmd logs -f yourarch"
 elif [ "$2" = "down" ]; then
-  $cmd down
+  run="$cmd down"
 elif [ "$2" = "up" ]; then
-  $cmd up -d
+  run="$cmd up -d"
 elif [ "$2" = "build" ]; then
-  $cmd build yourarch
+  run="$cmd build yourarch"
 elif [ "$2" = "rm" ]; then
-  sudo rm -rf database
+  run="sudo rm -rf database"
 elif [ "$2" = "pull" ]; then
-  git pull
+  run="git pull"
 fi
+
+eval $run
