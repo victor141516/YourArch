06c4afa13dca2905affba6d67e7f6ab09cd2fc71
Victor <yomellamovictor@gmail.com>
2022-01-07 02:58:58 +0100

    --wip-- [skip ci]

--- ctl.sh
+++ ctl.sh
@@ -1,16 +1,20 @@
 #!/bin/sh
 
 env=$1
-cmd="docker-compose -f docker-compose.base.yml -f docker-compose.$env.yml"
+compose_base="docker-compose -f docker-compose.base.yml -f docker-compose.$env.yml"
 
 if [ "$2" = "logs" ]; then
-  run="$cmd logs -f yourarch"
+  run="$compose_base logs -f yourarch"
 elif [ "$2" = "down" ]; then
-  run="$cmd down"
+  run="$compose_base down"
 elif [ "$2" = "up" ]; then
-  run="$cmd up -d"
+  run="$compose_base up -d"
 elif [ "$2" = "build" ]; then
-  run="$cmd build yourarch"
+  run="$compose_base build yourarch"
+elif [ "$2" = "migrate" ]; then
+  run="$compose_base exec yourarch npm run prisma:migrate -- --schema node_modules/.prisma/client/schema.prisma"
+elif [ "$2" = "psql" ]; then
+  run="$compose_base exec postgres psql -h127.0.0.1 -Upostgres postgres"
 elif [ "$2" = "rm" ]; then
   run="sudo rm -rf database"
 elif [ "$2" = "pull" ]; then
